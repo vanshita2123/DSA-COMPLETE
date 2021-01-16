7d8793222dbbfc10075606d9bad81f88117a7b6e
Vanshita <vanshita2123@gmail.com>
2021-01-16 23:48:18 +0530

    update

--- Recursion/main.c
+++ Recursion/main.c
@@ -65,7 +65,49 @@ double Taylor_iterative(int x, int n)
     return s;
 }
 
-void main()
+//fibonacci nth term
+int fib(int n)
 {
-    printf("%d",Rtaylor(5,20));
+    int t0=0,t1=1,s=0,i;
+
+    if(n<=1) return n;
+
+    for(i=2;i<=n;i++)
+    {
+        s=t0+t1;
+        t0=t1;
+        t1=s;
+    }
+
+    return s;
+}
+
+int F[10];
+
+//fibonacci nth term using recursion
+int mfib(int n)
+{
+    if(n<=1)
+    {
+        F[n]=n;
+        return n;
+    }
+    else
+    {
+        if(F[n-2]==-1)
+            F[n-2]=mfib(n-2);
+        if(F[n-1]==-1)
+            F[n-1]=mfib(n-1);
+        F[n]=F[n-2]+F[n-1];
+        return F[n-2]+F[n-1];
+    }
+}
+
+int main() {
+    int i;
+    for (i = 0; i < 10; i++)
+        F[i] = -1;
+
+    printf("%d \n", mfib(6));
+    return 0;
 }
